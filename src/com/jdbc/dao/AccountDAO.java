9083635cfefb02738f894e36d619fdfd662bda1f
Venkateshe <kolapallinagavenkatesh@gmail.com>
2022-10-07 15:29:31 +0530

    Added Statement

--- src/com/jdbc/dao/AccountDAO.java
+++ src/com/jdbc/dao/AccountDAO.java
@@ -9,6 +9,9 @@ public class AccountDAO {
 		try {
 			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/mydb", "root", "Venky1234@");
 			System.out.println(connection);
+			Statement statement = connection.createStatement();
+			int result = statement.executeUpdate("insert into account values(1,'Kolapalli','Venkatesh',10000)");
+			System.out.println(result + " rows got inserted");
 		} catch (SQLException e) {
 			e.printStackTrace();
 
