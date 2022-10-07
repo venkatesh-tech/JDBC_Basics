66df94822145abea4ec31aebc1ca657c880b2fc7
Venkateshe <kolapallinagavenkatesh@gmail.com>
2022-10-07 15:38:58 +0530

    Added DML Operations

--- src/com/jdbc/dao/AccountDAO.java
+++ src/com/jdbc/dao/AccountDAO.java
@@ -10,8 +10,19 @@ public class AccountDAO {
 			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/mydb", "root", "Venky1234@");
 			System.out.println(connection);
 			Statement statement = connection.createStatement();
-			int result = statement.executeUpdate("insert into account values(1,'Kolapalli','Venkatesh',10000)");
-			System.out.println(result + " rows got inserted");
+
+			// Insert
+//			int result = statement.executeUpdate("insert into account values(1,'Kolapalli','Venkatesh',10000)");
+//			System.out.println(result + " rows got inserted");
+
+			// Update
+//			int result = statement.executeUpdate("update account set bal=5000 where accno=1");
+//			System.out.println(result + " rows got Updated");
+
+			// Delete
+			int result = statement.executeUpdate("delete from account where accno=1");
+			System.out.println(result + " rows got deleted");
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 
